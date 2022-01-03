7b0c67c2b0e7152079a7c50776ea45f955382db2
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-01-03 08:04:26 -0300

    first commit

---
+++ src/main/java/com/softsaj/redelivre/RedelivreApplication.java
@@ -0,0 +1,13 @@
+package com.softsaj.redelivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedelivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedelivreApplication.class, args);
+	}
+
+}
